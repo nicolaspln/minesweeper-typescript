270bc9e1107b9bce84087136862a75b802d8113a
npoulain <nic.poulain@hotmail.fr>
2020-03-15 17:02:39 +0100

    Finished typescript minesweeper solo game

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import { CellState, CellValue } from '../../types';
+
+import './Button.scss';
+
+interface ButtonProps {
+  row: number;
+  col: number;
+  state: CellState;
+  value: CellValue;
+  red?: boolean;
+  onClick(
+    rowParam: number,
+    colParam: number,
+  ): (...args: any[]) => void;
+  onContext(
+    rowParam: number,
+    colParam: number,
+  ): (...args: any[]) => void;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  col,
+  onClick,
+  onContext,
+  row,
+  state,
+  value,
+  red,
+}) => {
+  const renderContent = (): React.ReactNode => {
+    if (state === CellState.visible) {
+      if (value === CellValue.bomb) {
+        return (
+          <span role="img" aria-label="bomb">
+            💣
+          </span>
+        );
+      } else if (value === CellValue.none) {
+        return null;
+      }
+      return value;
+    } else if (state === CellState.flagged) {
+      return (
+        <span role="img" aria-label="flag">
+          🚩
+        </span>
+      );
+    }
+
+    return null;
+  };
+
+  return (
+    <div
+      className={`Button ${
+        state === CellState.visible ? 'visible' : ''
+      } value-${value} ${red ? 'red' : ''}`}
+      onClick={onClick(row, col)}
+      onContextMenu={onContext(row, col)}
+    >
+      {renderContent()}
+    </div>
+  );
+};
+
+export default Button;
